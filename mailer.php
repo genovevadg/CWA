9dd06014af7c98d38d1864e2e5ffa806b4828637
Martin Ruffa <mruffa@HEXACTA.COM>
2020-11-04 21:16:24 -0300

    fixing email

--- mailer.php
+++ mailer.php
@@ -2,8 +2,9 @@
     
     $mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
     $from = $_POST["contact-type"];
+    $finalMssg = "Mi numero es: " . $_POST["phone"] ."\r\n" . $_POST["message"];
     
-      if(mail($from, $_POST["subject"], $_POST["message"], $mailHeaders)) {
+      if(mail($from, $_POST["subject"], $finalMssg, $mailHeaders)) {
         if(strpos($from, 'bbva') !== FALSE){
           header('Location: http://cwa.com.ar/bbva_contacto_exito.html', true, 301);
         } else {
